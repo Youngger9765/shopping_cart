005c7ad0038a6ce4e5a029a7e30f96b5265af1d2
purpleice9765 <purpleice9765@msn.com>
2015-11-10 11:01:17 +0800

    admin product create

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -8,4 +8,17 @@ class Admin::ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    flash[:notice] = "product 新增成功"
+    redirect_to admin_products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :unit, :price)
+  end
+
 end
