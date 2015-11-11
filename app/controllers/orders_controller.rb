f40212bfd2cd74bab6804c657f3c4113ef3dff2e
purpleice9765 <purpleice9765@msn.com>
2015-11-11 12:37:46 +0800

    update product in order price information

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,37 +10,44 @@ class OrdersController < ApplicationController
     @order = Order.new(order_params)
     @order.save
 
-    if params[:order][:product_list]
-      product_list = params[:order][:product_list]
-      product_list.shift(1)
-
-      product_list.each do|p|
-        product_id = p.to_i
-        OrderProductShip.create( :order_id => @order.id, :product_id => product_id)
-      end
-    end
-
-
-    redirect_to orders_path
+    redirect_to order_path(@order)
   end
 
   def show
     @order = Order.find(params[:id])
+
   end
 
   def update
     @order = Order.find(params[:id])
     Order.find(params[:id]).update(order_params)
 
-    if params[:order][:product_list]
+    if params[:order][:product_list] && params[:order][:order_number] !=""
       product_list = params[:order][:product_list]
       product_list.shift(1)
 
+      number =params[:order][:order_number].to_i
+
       product_list.each do|p|
         product_id = p.to_i
-        OrderProductShip.create( :order_id => params[:id], :product_id => product_id)
+
+        if OrderProductShip.where(:order_id => @order.id).find_by_product_id(product_id)
+
+        else
+          OrderProductShip.create( :order_id => params[:id], :product_id => product_id)
+        end
+        ships = OrderProductShip.where(:order_id => @order.id).find_by_product_id(product_id)
+        ships.number = number
+
+        product_price = Product.find(product_id).price
+
+        ships.subtotal = product_price * number
+        ships.save!
       end
     end
+
+
+
 
     redirect_to order_path(@order)
   end
@@ -49,7 +56,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:name, :product_list)
+    params.require(:order).permit(:name, :product_list, :phone, :address)
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,17 +3,25 @@ class Order < ActiveRecord::Base
   has_many :order_product_ships
   has_many :products, :through => :order_product_ships
 
+  def order_number
+  end
+
   def product_list
     self.products.map{|p| p.name}
   end
 
-  def product_list=(str)
-    arr = str.split(",")
-  
-    self.products = arr.map do |p|
-      product = products.find_by_name(p)
-      product
-    end
+  def product_number(p)
+    ships = OrderProductShip.where(:order_id => self.id).find_by_product_id(p.id)
+    number = ships.number
   end
 
+  def product_subtotal(p)
+    ships = OrderProductShip.where(:order_id => self.id).find_by_product_id(p.id)
+    subtotal = ships.subtotal 
+  end
+
+  def total_price
+    ships = OrderProductShip.where(:order_id => self.id)
+    ships.to_a.sum(&:subtotal)
+  end
 end
